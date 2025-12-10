4617195c0cf7a4b64d328893d855709b5984ebb5
carlitomm <cmiguelezmachado@gmail.com>
2025-12-09 19:32:05 -0500

    commits

--- todo-app-frontend/src/components/FilterBar.tsx
+++ todo-app-frontend/src/components/FilterBar.tsx
@@ -1,33 +1,38 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../store/store";
 import { setFilter } from "../store/slices/todoSlice";
+import { Bars3Icon, ClockIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 export default function FilterBar() {
     const dispatch = useDispatch();
     const filter = useSelector((state: RootState) => state.todos.filter);
 
     const options = [
-        { id: "all", label: "All" },
-        { id: "active", label: "Active" },
-        { id: "completed", label: "Completed" }
+        { id: "all", label: "All", icon: Bars3Icon },
+        { id: "active", label: "Active", icon: ClockIcon },
+        { id: "completed", label: "Completed", icon: CheckIcon }
     ];
 
     return (
-        <div className="flex gap-3">
-            {options.map(opt => (
-                <button
+        <div className="flex gap-6 justify-center">
+            {options.map(opt => {
+
+                const Icon = opt.icon;
+
+                return (<button
                     key={opt.id}
                     onClick={() => dispatch(setFilter(opt.id as any))}
-                    className={`
-            px-4 py-2 rounded-lg font-medium border
-            ${filter === opt.id
-                            ? "bg-blue-600 text-white border-blue-600"
-                            : "bg-white text-gray-700 hover:bg-gray-100"}
-          `}
+                    className={`relative font-medium transition text-gray-600 hover:text-blue-500 flex items-center gap-1
+                        ${filter === opt.id ? "text-blue-600" : ""}
+                    `}
                 >
+                    <Icon className="h-4 w-4" />
                     {opt.label}
-                </button>
-            ))}
+                    {filter === opt.id && (
+                        <span className="absolute left-0 right-0 -bottom-1 mx-auto h-[2px] w-full bg-blue-600 rounded-full"></span>
+                    )}
+                </button>)
+            })}
         </div>
     );
 }
